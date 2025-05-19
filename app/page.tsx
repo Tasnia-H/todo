8935bf6cc629eab8f4f9503b94d165bfab73c987
Tasnia-H <tasniahossain2014@gmail.com>
2025-05-20 01:47:22 +0600

    test-2

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
       {session ? (
         <>
           <div className="text-center my-10">
-            <h1 className="text-2xl font-bold my-10">Todo List</h1>
+            <h1 className="text-2xl font-bold my-10">Todo List!</h1>
             <AddTask />
           </div>
           <TodoList tasks={tasks} />
